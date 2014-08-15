067b57a6c84cdf6a9d1d2b19de0d963ea5d4f222
Sonar Systems <admin@sonarsystems.co.uk>
2014-08-15 09:58:17 +0100

    Rotating A Sprite

---
+++ TutorialSFML/TutorialSFML/main.cpp
@@ -0,0 +1,48 @@
+#include "SFML/Graphics.hpp"
+#include <iostream>
+
+int main()
+{
+	sf::RenderWindow window( sf::VideoMode( 600, 600 ), "SFML WORK!" );
+
+	sf::Texture texture;
+
+	if (!texture.loadFromFile("Paddle Large.png"))
+	{
+		std::cout << "Error loading paddle texture :(" << std::endl;
+	}
+
+	//sf::Sprite sprite(texture);
+
+	// or
+
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+
+	sprite.rotate(15);
+	sprite.rotate(15);
+	sprite.rotate(15);
+
+	while ( window.isOpen( ) )
+	{
+		sf::Event event;
+
+		while ( window.pollEvent( event ) )
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+
+				break;
+
+			}
+		}
+
+		window.clear( );
+
+		window.draw(sprite);
+
+		window.display( );
+	}
+}
